c1dc5c2e5245a8f51a0b366ff0d73c29eaa8a871
Gilang Anggara <whirlpool27@gmail.com>
2018-08-02 16:39:32 +0700

    Add basic apps

---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title':'Article One',
+            'body':'Some paragraph',
+            'author':'Gilang Anggara',
+            'create_date':'02-08-2018'
+        },
+        {
+            'id': 2,
+            'title':'Article Two',
+            'body':'Some paragraph',
+            'author':'Gilang Anggara',
+            'create_date':'02-08-2018'
+        },
+        {
+            'id': 3,
+            'title':'Article Three',
+            'body':'Some paragraph',
+            'author':'Gilang Anggara',
+            'create_date':'02-08-2018'
+        }
+    ]
+
+    return articles
